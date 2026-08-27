8de54ac18788763b8c8b2450d65e80cae2f67351
Rhodney Desamours <rdesamours2023@fau.edu>
2026-08-27 15:59:07 -0400

    Initial commit

---
+++ Homework1/p1_Desamours_Rhodney.py
@@ -0,0 +1,26 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = 0
+b = 0
+c = 0
+
+while a != "":
+    a = input("Enter coefficient a: ")
+    a = float(a)
+    b = float(input("Enter coefficient b: "))
+    c = float(input("Enter coefficient c: "))
+
+    discrim = b**2 - 4 * a * c
+
+    if discrim < 0:
+        print("no real solutions")
+    elif discrim == 0:
+        x1 = (-b)/(2 * a)
+        print("one solution:", x1)
+    elif discrim > 0:
+        x1 = (-b + math.sqrt(b**2 - 4 * a * c))/(2 * a)
+        x2 = (-b - math.sqrt(b**2 - 4 * a * c))/(2 * a)
+        print("two solutions:", x1, ",", x2)
+
